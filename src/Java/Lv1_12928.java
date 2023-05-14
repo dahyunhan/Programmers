3efb5e54272d89d28e2ad2d254f1809af9c99068
dahyunhan <dollae7020@gmail.com>
2023-05-14 20:54:15 +0900

    Lv1_12928.java

---
+++ src/Java/Lv1_12928.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class Lv1_12928 {
+    public int solution(int n) {
+
+        int sum = 0;
+
+        for (int i = 1; i < n + 1; i++) {
+
+            if (n % i == 0) {
+
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
